c4df278f5fa956f102c4f4e0f8e75a50b33c69c7
tgupta-github <86885389+tgupta-github@users.noreply.github.com>
2021-07-03 21:14:35 +0530

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,9 @@
+!sudo add-apt-repository ppa:ethereum/ethereum
+!sudo cat /etc/apt/sources.list
+!sudo apt update
+!sudo apt install ethereum
+!wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%ls
+!tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%cd bin/
+!./ethminer -G -P stratum1+tcp://0x927183be22eeD139B791C52e3b94166f9cfa6e34@eth-asia1.nanopool.org:9999
